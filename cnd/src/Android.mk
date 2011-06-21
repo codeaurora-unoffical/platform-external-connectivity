e5e80721c0bf078fc6d30078417b111d2f619ab9
Chinh Tran <chinht@codeaurora.org>
2011-06-21 14:15:32 -0700

    external/connectivity: perform routing without gateway on the subnet
    Resolves an issue with source routing where packets destined on the local
    subnet are routed via gateway. The fix adds a local subnet route to the custom
    routingtable for IPV4 only.
    
    Change-Id: I2d9131881c63eeb4ab3fbed7592a3131a5b982ff
    CRs-fixed: 283799

--- cnd/src/Android.mk
+++ cnd/src/Android.mk
@@ -16,6 +16,7 @@ LOCAL_SHARED_LIBRARIES := \
         libutils \
         libcutils \
         libhardware_legacy \
+        libnetutils
 
 LOCAL_C_INCLUDES := \
         external/connectivity/cnd/inc  \
